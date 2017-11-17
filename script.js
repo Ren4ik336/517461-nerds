b8515ae32d57857527fe4082f22d17a8bd3eaee9
Ren4ik336 <atnashev@mail.ru>
2017-11-18 02:30:14 +0300

    попап

---
+++ script.js
@@ -0,0 +1,16 @@
+var element = document.querySelector("a[class='button contacts-button']");
+var close = document.querySelector("span[class='modal-close'");
+if (element) {
+    
+    // TODO: Attaching sample click listener. Remove it.
+    element.addEventListener('click', function () {
+        document.getElementById('modal').style.display='block';
+    }, false);
+  
+    
+}
+if (close) {
+	   close.addEventListener('click', function (){
+     	document.getElementById('modal').style.display='none';
+     }, false);
+}
