f5e7c3380fbc79fe76a2674cddf780032223e1a8
Ren4ik336 <atnashev@mail.ru>
2017-11-19 02:03:21 +0300

    попап 2

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var element = document.querySelector("a[class='button contacts-button']");
 var close = document.querySelector("span[class='modal-close'");
+var active = document.querySelector("section[class='write-us-hidden']")
 if (element) {
     
     // TODO: Attaching sample click listener. Remove it.
-    element.addEventListener('click', function () {
-        document.getElementById('modal').style.display='block';
+   element.addEventListener('click', function () {
+    	//active.classList.add("active");
+       document.getElementById('modal').style.display='block';
+        
     }, false);
   
     
